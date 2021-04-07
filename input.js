359f5a8e4d9ed25aae1ee9cf37fc4de4e9ab4e0d
Tyler Bratt <tyler~bratt@hotmail.com>
2021-04-07 18:34:24 -0400

    reupp

--- input.js
+++ input.js
@@ -1,11 +1,26 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
-  stdin.setEncode('utf-8');
+  stdin.setEncoding('utf-8');
   stdin.resume();
   stdin.on(handleUserInput, (key)=> {
     if (key === '\u0003') {
       process.exit();
+    }
+    if (key === '\u0077') {
+      console.log('up');
+    }
+    if (key === '\u0073') {
+      console.log('down');
+    }
+    if (key === '\u0061') {
+      console.log('left');
+    }
+    if (key === '\u0064') {
+      console.log('right');
     }
   });
   return stdin;
