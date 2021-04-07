cc2676df1c8e66a11e4b7993e7c5d90ef54cf713
Tyler Bratt <tyler~bratt@hotmail.com>
2021-04-07 14:19:16 -0400

    modified play.js

--- play.js
+++ play.js
@@ -1,3 +1,21 @@
 const connect = require('./client');
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncode('utf-8');
+  stdin.resume();
+  stdin.on(handleUserInput, (key)=> {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+};
+
+const handleUserInput = function() {
+
+};
+
 console.log('Connecting...');
 connect();
